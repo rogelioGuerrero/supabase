fd23e5cd20ee39f77174b82ab4ed3019cc28d620
Rogelio Guerrero <rogelio.guerrero@example.com>
2025-02-19 15:13:30 -0600

    Agregar logging en serverless function para debugging

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,6 +1,18 @@
 const serverless = require('serverless-http');
 const app = require('../../dist/app').default;
 
+console.log('Iniciando serverless function');
+console.log('Configuración de app:', app);
+
 module.exports.handler = serverless(app, {
   binary: ['*/*']
+});
+
+// Logging de errores
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
 });
